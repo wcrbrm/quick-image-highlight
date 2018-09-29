b54973976e73cd87d33399ff4e8f2593add6e798
wcrbrm <viclopata@gmail.com>
2018-09-29 20:28:11 +0300

    more on top panel look - prototype of buttons

--- src/main/scala/com/webcerebrium/quick_image/TopPanel.scala
+++ src/main/scala/com/webcerebrium/quick_image/TopPanel.scala
@@ -8,6 +8,8 @@ import scalafx.scene.layout.{ FlowPane, BorderPane, HBox, VBox }
 import scalafx.event.{ ActionEvent, EventHandler }
 import scalafx.scene.control._
 import scalafx.Includes._
+import javafx.scene.{control => jfxsc, text => jfxst}
+import javafx.{scene => jfxs}
 
 trait TopPanelTrait {
   def get:BorderPane
@@ -37,17 +39,50 @@ case class TopPanelNoImage(onUpdate: () => Unit) extends TopPanelTrait {
 }
 
 case class TopPanelWithImage(onUpdate: () => Unit) extends TopPanelTrait {
+
+  var drawingMode = "crop"
+  val radioToggleGroup = new ToggleGroup
+  def getMode:String = radioToggleGroup.selectedToggle().asInstanceOf[jfxsc.RadioButton].text()
+  def onUpdateMode = { println("drawing mode changed to " + getMode); }
+  
   val btnReset = new Button("Reset") { 
+    style = "-fx-background-color: #fff"
     onAction = handle { 
         CurrentImage.reset 
         onUpdate()
     }
   }
+  val btnSave = new Button("Save") { 
+    style = "-fx-background-color: #faa; -fx-cursor: pointer"
+    onAction = handle { println("Consider All Saved") }
+  }
+
   override def get = new BorderPane {
     style = "-fx-background-color: #eee"
     padding = Insets(10, 10, 10, 10)
-    left = new HBox {
-        children = btnReset
+    left = new HBox(8) {
+      children = List(
+        new RadioButton("CROP") {
+            toggleGroup = radioToggleGroup
+            selected = true
+            onAction = handle { onUpdateMode }
+        },
+        new RadioButton("LINE") {
+            toggleGroup = radioToggleGroup
+            onAction = handle { onUpdateMode }
+        },
+        new RadioButton("ARROW") {
+            toggleGroup = radioToggleGroup
+            onAction = handle { onUpdateMode }
+        },
+        new RadioButton("BOX") {
+            toggleGroup = radioToggleGroup
+            onAction = handle { onUpdateMode }
+        }
+      )
+    }
+    right = new HBox(20) {
+      children = List(btnReset, btnSave)
     }
   }
 }
