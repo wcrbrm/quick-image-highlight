b7f853ef3a52ade9d8eeda24da04ff17e53b816e
wcrbrm <viclopata@gmail.com>
2018-10-05 22:30:54 +0300

    mouse point detection

--- src/main/scala/com/webcerebrium/quick_image/CurrentImage.scala
+++ src/main/scala/com/webcerebrium/quick_image/CurrentImage.scala
@@ -5,6 +5,10 @@ import scalafx.geometry.{ Insets, Pos }
 import scalafx.scene.image.{ Image, ImageView }
 import scalafx.scene.layout.{ BorderPane, HBox, VBox }
 import scalafx.scene.control._
+import javafx.geometry.Point2D
+import javafx.scene.input.MouseEvent
+import javafx.event.EventHandler
+
 import java.awt.{Toolkit, Graphics2D}
 import java.awt.datatransfer.{ Clipboard, DataFlavor, UnsupportedFlavorException }
 import java.awt.image.BufferedImage
@@ -17,6 +21,9 @@ import javax.imageio.ImageIO
 object CurrentImage {
 
   var bufferedImage: Option[BufferedImage] = None
+  var startPoint: Option[Point2D] = None
+  var mode: String = "crop"
+  var onUpdate: () => Unit = () => {}
 
   def fromClipboard: Unit = {
     try {
@@ -40,13 +47,41 @@ object CurrentImage {
   def isPresent = bufferedImage.isDefined
   def save(f: File) = { bufferedImage.map(bim => ImageIO.write(bim, "jpg", f)) }
 
+  def applyTool(p1: Point2D, p2: Point2D) = {
+
+     val g2d:Graphics2D = bufferedImage.get.createGraphics
+     val minX = Math.min(p1.getX, p2.getX).toInt
+     val minY = Math.min(p1.getY, p2.getY).toInt
+     val maxX = Math.max(p1.getX, p2.getX).toInt
+     val maxY = Math.max(p1.getY, p2.getY).toInt
+
+     g2d.fillRect(minX, minY, maxX - minX, maxY - minY)
+     println("apply tool: " + mode + ", p1=" + p1.toString + ", p2=" + p2.toString)
+  }
+
   def getImageView: ImageView = {
     if (bufferedImage.isDefined) {
 
-      val g2d:Graphics2D = bufferedImage.get.createGraphics
-      g2d.fillRect(0, 0, 200, 200)
+      // val g2d:Graphics2D = bufferedImage.get.createGraphics
+      // g2d.fillRect(0, 0, 200, 200)
 
-      new ImageView { image = SwingFXUtils.toFXImage(bufferedImage.get, null) }
+      new ImageView { 
+        image = SwingFXUtils.toFXImage(bufferedImage.get, null) 
+      
+        onMouseClicked = new EventHandler[MouseEvent] {
+	        override def handle(event: MouseEvent) {
+            if (startPoint.isDefined) {
+              applyTool(startPoint.get, new Point2D(event.getX, event.getY))
+              startPoint = None;
+            } else {
+              startPoint = Some(new Point2D(event.getX, event.getY))
+              println("startPoint = ", startPoint.toString)
+            }
+            onUpdate()
+	        }
+	      }
+      }
+
     } else {
       ContentNoImage.getImageView
     }
